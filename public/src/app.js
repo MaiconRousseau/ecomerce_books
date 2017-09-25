35da94404d3fa90da93b84cb32d4ea78fe666184
MaiconRousseau <dionmaicon@outlook.com>
2017-09-25 17:57:36 -0300

    Exercicio Material Succes

--- public/src/app.js
+++ public/src/app.js
@@ -63,14 +63,15 @@ function limparTabela(){
 var drawerEl = document.querySelector('.mdc-temporary-drawer');
 var MDCTemporaryDrawer = mdc.drawer.MDCTemporaryDrawer;
 var drawer = new MDCTemporaryDrawer(drawerEl);
+
 document.querySelector('.menu').addEventListener('click', function() {
   drawer.open = true;
 });
 drawerEl.addEventListener('MDCTemporaryDrawer:open', function() {
-  console.log('Received MDCTemporaryDrawer:open');
+
 });
 drawerEl.addEventListener('MDCTemporaryDrawer:close', function() {
-  console.log('Received MDCTemporaryDrawer:close');
+
 });
 
 let menu = new mdc.menu.MDCSimpleMenu(document.querySelector('.mdc-simple-menu'));
